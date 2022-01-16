27cb9aeaa3936cd895c41835d63d0ab8b8a76ee3
Greg Borrelly <gregborrelly@gmail.com>
2022-01-16 15:59:19 -0500

    test

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ export default function Home(props) {
   const { IP,  } = props;
   return (
     <div className={styles.container}>
-        <h1>YOUR IP is {IP}</h1>
+        <h1>YOUR IP is  {IP}</h1>
     </div>
   )
 }
 
 export async function getServerSideProps({ req, res, params }) {
-  const IP = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();;
+  const IP = (req.headers['x-forwarded-for'] ||  '').split(',')[0].trim();;
   console.log(req)
 
   return {
