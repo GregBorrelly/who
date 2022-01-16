e7b9a67f23db27e50eb81aff7facbcba052941e8
Greg Borrelly <gregborrelly@gmail.com>
2022-01-16 17:22:32 -0500

    last update

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,46 @@
 import styles from '../styles/Home.module.css'
 import Terminal, { ColorMode, LineType } from 'react-terminal-ui';
 
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default function Home(props) {
   const { IP,data  } = props;
+  debugger;
   const dataArr = Object.keys(data);
-  const terminalData = dataArr.map( key => {
+  const terminalData = dataArr.filter(key => {
+    if(key == 'ip' || key == 'country_flag' ) return null;
+    else return key;
+  }).map( key => {
     const datafield = data[key];
     return ({
       type: LineType.Output, value: `${key} : ${datafield}`})
   })
+  terminalData.unshift({type: LineType.Input, value: `echo data for IP: ${IP}`});
+
+  const defaultProps = {
+    center: {
+      lat: Number(data.latitude),
+      lng: Number(data.longitude)
+    },
+    zoom: 10
+  };
   debugger;
   return (
     <div className={styles.container}>
+            {/* <div style={{ height: '20vh', width: '30%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyB2vk-flSwNsDj30zDY6RKAMxCICYOw5MY' }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        >
+        </GoogleMapReact>
+      </div> */}
 
-    <Terminal name='Gregs terminal' colorMode={ ColorMode.Light }  lineData={terminalData} onInput={ terminalInput => console.log(`New terminal input received: '${ terminalInput }'`) }/>
+      {/* <img src={data['country_flag']} /> */}
+
+    <Terminal name='USER IDENTIFICATION SERVICE'   lineData={terminalData} onInput={ terminalInput => console.log(`New terminal input received: '${ terminalInput }'`) }/>
 
           
     </div>
@@ -24,10 +50,7 @@ export default function Home(props) {
 export async function getServerSideProps({ req, res, params }) {
   const IP = (req.headers['x-forwarded-for'] ||  '24.146.137.46').split(',')[0].trim();
   const request = await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=823e20bde8f34d41b9f4757f6b13dca8&ip=${IP}`);
-  const data = await request.json()
-  debugger;
-  console.log(req)
-
+  const data = await request.json();
   return {
     props: {
       IP,
