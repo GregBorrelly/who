c1d6667dfb3e99e42eb0ed5affdfd92c65462af7
Greg Borrelly <gregborrelly@gmail.com>
2022-01-16 15:55:33 -0500

    second attempt

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home(props) {
-  const { IP } = props;
+  const { IP,  } = props;
   return (
     <div className={styles.container}>
         <h1>YOUR IP is {IP}</h1>
@@ -12,12 +12,12 @@ export default function Home(props) {
 }
 
 export async function getServerSideProps({ req, res, params }) {
-  const IP = req.socket.remoteAddress;
-  console.log(IP)
+  const IP = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();;
+  console.log(req)
 
   return {
     props: {
-      IP
+      IP,
     }
   }
 }
